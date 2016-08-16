0219a06edeae670bc606ed1a8073403910ee8ff2
Alinshans <13925588730@163.com>
2016-08-16 17:40:19 +0800

    Accomplished iterator.h

--- MyTinySTL/iterator.h
+++ MyTinySTL/iterator.h
@@ -126,5 +126,55 @@ namespace MyTinySTL {
 		value_type(const Iterator&) {
 		return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
 	}
+	
+	// distance 函数：迭代器的距离
+	template <class InputIterator>
+	inline iterator_traits<InputIterator>::difference_type
+		distance(InputIterator first, InputIterator last) {
+		typedef typename iterator_traits<InputIterator>::iterator_category category;
+		return __distance(first, last, category());
+	}
+
+	template <class InputIterator>
+	inline iterator_traits<InputIterator>::difference_type
+		__distance(InputIterator first, InputIterator last, input_iterator_tag) {
+		iterator_traits<InputIterator>::difference_type n = 0;
+		while (first != last) {
+			++first;
+			++n;
+		}
+		return n;
+	}
+
+	template <class RandomAccessIterator>
+	inline iterator_traits<RandomAccessIterator>::difference_type
+		__distance(RandomAccessIterator first, RandomAccessIterator last,
+			random_access_iterator_tag) {
+		return last - first;
+	}
+
+	// advance 函数：迭代器前进 n 个距离
+	template <class InputIterator, class Distance>
+	inline void advance(InputIterator& i, Distance n) {
+		__advance(i, n, iterator_category(i));
+	}
+
+	template <class InputIterator, class Distance>
+	inline void __advance(InputIterator& i, Distance n, input_iterator_tag) {
+		while (n--)	++i;
+	}
+
+	template <class BidirectionalIterator, class Distance>
+	inline void __advance(BidirectionalIterator& i, Distance n, bidirectional_iterator_tag) {
+		if (n >= 0)
+			while (n--)	++i;
+		else
+			while (n++)	--i;
+	}
+
+	template <class RandomAccessIterator, class Distance>
+	inline void __advance(RandomAccessIterator& i, Distance n, random_access_iterator_tag) {
+		i += n;
+	}
 }
 #endif // !ITERATOR_H
