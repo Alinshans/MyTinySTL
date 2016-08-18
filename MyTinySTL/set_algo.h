c9462eddf9d77846e4d1d635c6b1ae8771b22346
Alinshans <13925588730@163.com>
2016-08-18 10:38:04 +0800

    Update set_algo.h

--- MyTinySTL/set_algo.h
+++ MyTinySTL/set_algo.h
@@ -8,8 +8,10 @@
 //接受 set/multiset 容器作为输入区间
 namespace MyTinySTL {
 
-	/***************************** set_union *********************************/
-	//计算S1∪S2的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
+	// set_union
+	// 计算S1∪S2的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
 	template <class InputIterator1, class InputIterator2, class OutputIterator>
 	OutputIterator set_union(InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
@@ -33,8 +35,35 @@ namespace MyTinySTL {
 		return copy(first2, last2, copy(first1, last1, result));
 	}
 
-	/****************************** set_intersection *****************************/
-	//计算S1∩S2的结果并保存到result中，返回输出区间的尾端
+	// 重载版本使用仿函数 comp 代替比较操作
+	template <class InputIterator1, class InputIterator2, class OutputIterator,
+	class Compared>
+	OutputIterator set_union(InputIterator1 first1, InputIterator1 last1,
+		InputIterator2 first2, InputIterator2 last2, OutputIterator result,Compared comp) {
+		while (first1 != last1 && first2 != last2) {
+			if (comp(*first1, *first2)) {
+				*result = *first1;
+				++first1;
+			}
+			else if (comp(*first2, *first1)) {
+				*result = *first2;
+				++first2;
+			}
+			else {
+				*result = *first1;
+				++first1;
+				++first2;
+			}
+			++result;
+		}
+		//将剩余元素拷贝到 result
+		return copy(first2, last2, copy(first1, last1, result));
+	}
+
+	/*********************************************************************************/
+	// set_intersection
+	// 计算S1∩S2的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
 	template <class InputIterator1, class InputIterator2, class OutputIterator>
 	OutputIterator set_intersection(InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
@@ -51,7 +80,7 @@ namespace MyTinySTL {
 		}
 	}
 
-	//使用仿函数 comp 比较
+	// 重载版本使用仿函数 comp 代替比较操作
 	template <class InputIterator1, class InputIterator2, class OutputIterator, 
 		class Compared>
 	OutputIterator set_intersection(InputIterator1 first1, InputIterator1 last1,
@@ -70,8 +99,10 @@ namespace MyTinySTL {
 		}
 	}
 
-	/***************************** set_difference ****************************/
-	//计算S1-S2的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
+	// set_difference
+	// 计算S1-S2的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
 	template <class InputIterator1, class InputIterator2, class OutputIterator>
 	OutputIterator set_difference(InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
@@ -92,7 +123,7 @@ namespace MyTinySTL {
 		return copy(first1, last1, result);
 	}
 
-	//使用仿函数 comp 比较
+	// 重载版本使用仿函数 comp 代替比较操作
 	template <class InputIterator1, class InputIterator2, class OutputIterator,
 		class Compared>
 	OutputIterator set_difference(InputIterator1 first1, InputIterator1 last1,
@@ -115,8 +146,10 @@ namespace MyTinySTL {
 		return copy(first1, last1, result);
 	}
 
-	/**************************** set_symmetric_difference ***************************/
-	//计算(S1-S2)∪(S2-S1)的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
+	// set_symmetric_difference
+	// 计算(S1-S2)∪(S2-S1)的结果并保存到result中，返回输出区间的尾端
+	/*********************************************************************************/
 	template <class InputIterator1, class InputIterator2, class OutputIterator>
 	OutputIterator set_symmetric_difference(InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
@@ -139,7 +172,7 @@ namespace MyTinySTL {
 		return copy(first2, last2, copy(first1, last1, result));
 	}
 
-	//使用仿函数 comp 比较
+	// 重载版本使用仿函数 comp 代替比较操作
 	template <class InputIterator1, class InputIterator2, class OutputIterator,
 		class Compared>
 	OutputIterator set_symmetric_difference(InputIterator1 first1, InputIterator1 last1,
