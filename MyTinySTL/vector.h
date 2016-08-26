2916a4eec2f95cfccb0ca91520a10aaa95956722
Alinshans <13925588730@163.com>
2016-08-26 21:31:45 +0800

    Update vector.h

--- MyTinySTL/vector.h
+++ MyTinySTL/vector.h
@@ -79,12 +79,20 @@ namespace MyTinySTL {
 		reference back() { return *(end() - 1); }
 
 		// 修改容器相关操作
+		void assign(size_type n, const T& value) { fill_assign(n, value); }
+		void assign(size_type n) { fill_assign(n, T()); }
+		template <class InputIterator>
+		void assign(InputIterator first, InputIterator last);
 		void push_back(const T& x);
 		void pop_back();
 		iterator earse(iterator position);
 		iterator earse(iterator first, iterator last);
 		void clear() { earse(begin(), end()); }
+		iterator insert(iterator position, const T& x);
+		iterator insert(iterator position);
 		void insert(iterator position, size_type n, const T& x);
+		template <class InputIterator>
+		void insert(iterator position, InputIterator first, InputIterator last);
 		void swap(vector<T, Alloc>& x);
 
 		// 配置器相关操作
@@ -98,8 +106,30 @@ namespace MyTinySTL {
 		void vector_construct(InputIterator first, InputIterator last, __false_type);
 		void destroy_and_deallocate();
 		void allocate_and_fill(size_type n, const T& value);
-		void allocate_and_copy(iterator first, iterator last);
+		template <class InputIterator>
+		void allocate_and_copy(InputIterator first, InputIterator last);
+		void fill_assign(size_type n, const T& value);
+		template <class Integer>
+		void assign_dispatch(Integer n, Integer value, __true_type);
+		template <class InputIterator>
+		void assign_dispatch(InputIterator first, InputIterator last, __false_type);
+		template <class InputIterator>
+		void assign_aux(InputIterator first, InputIterator last, input_iterator_tag);
+		template <class ForwardIterator>
+		void assign_aux(ForwardIterator first, ForwardIterator last, forward_iterator_tag);
 		void insert_aux(iterator position, const T& x);
+		template <class Integer>
+		void insert_dispatch(iterator position, Integer n, Integer x, __true_type);
+		template <class InputIterator>
+		void insert_dispatch(iterator position, InputIterator first, InputIterator last,
+			__false_type);
+		void fill_insert(iterator position, size_type n, const T& x);
+		template <class InputIterator>
+		void range_insert(iterator position, InputIterator first, InputIterator last,
+			input_iterator_tag);
+		template <class ForwardIterator>
+		void range_insert(iterator position, ForwardIterator first, ForwardIterator last,
+			forward_iterator_tag);
 	};
 
 	/********************************************************************************/
@@ -127,12 +157,12 @@ namespace MyTinySTL {
 
 	// 复制构造函数
 	template <class T, class Alloc>
-	vector<T, Alloc>::vector(const vector<T, Alloc>& x) {
-		allocate_and_copy(x.begin(), x.end());
+	vector<T, Alloc>::vector(const vector& x) {
+		allocate_and_copy(x.start, x.finish);
 	}
 
 	template <class T, class Alloc>
-	vector<T, Alloc>::vector(vector<T, Alloc>&& x) {
+	vector<T, Alloc>::vector(vector&& x) {
 		start = x.start;
 		finish = x.finish;
 		end_of_storage = x.end_of_storage;
@@ -151,12 +181,13 @@ namespace MyTinySTL {
 			else if (size() >= xlen) {	//如果原 vector 大小大于等于要赋值的 vector 大小
 				iterator i = copy(x.begin(), x.end(), begin());
 				destroy(i, finish);
+				finish = start + xlen;
 			}
 			else { //原 vector 大小小于要赋值的 vector 大小
 				copy(x.begin(), x.begin() + size(), start);
 				uninitialized_copy(x.begin() + size(), x.end(), finish);
+				end_of_storage = finish = start + xlen;
 			}
-			finish = start + xlen;
 		}
 		return *this;
 	}
@@ -180,6 +211,14 @@ namespace MyTinySTL {
 			earse(begin() + new_size, end());
 		else
 			insert(end(), new_size - size(), x);
+	}
+
+	// 初始化容器
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::assign(InputIterator first, InputIterator last) {
+		typedef typename __is_integer<InputIterator>::is_integer integer;
+		assign_dispatch(first, last, integer());
 	}
 
 	// 在容器尾端插入元素
@@ -216,8 +255,7 @@ namespace MyTinySTL {
 	typename vector<T, Alloc>::iterator
 		vector<T, Alloc>::earse(iterator first, iterator last) {
 		iterator i = copy(last, finish, first);	//把last之后的元素复制到first为起始的空间
-		destroy(i, finish);	//析构清除不要的元素
-		data_allocator::deallocate(i, finish);
+		destroy(i, finish);	//销毁不要的元素
 		finish = finish - (last - first);	//调整水位
 		return first;
 	}
@@ -225,20 +263,202 @@ namespace MyTinySTL {
 	// 与另一个 vector 交换
 	template <class T, class Alloc>
 	void vector<T, Alloc>::swap(vector<T, Alloc>& x) {
-		swap(start, x.start);
-		swap(finish, x.finish);
-		swap(end_of_storage, x.end_of_storage);
+		if (*this != x) {
+			MyTinySTL::swap(start, x.start);
+			MyTinySTL::swap(finish, x.finish);
+			MyTinySTL::swap(end_of_storage, x.end_of_storage);
+		}
 	}
 
-	// 重载 MyTinySTL 的 swap
+	// 在 position 位置插入元素									 
 	template <class T, class Alloc>
-	inline void swap(vector<T, Alloc>& x, vector<T, Alloc>& y) {
-		x.swap(y);
+	typename vector<T, Alloc>::iterator
+		vector<T, Alloc>::insert(iterator position, const T& x) {
+		size_type n = position - begin();
+		if (finish != end_of_storage && position == end()) {
+			construct(finish, x);
+			++finish;
+		}
+		else
+			insert_aux(position, x);
+		return begin() + n;
 	}
-	
+
+	template <class T, class Alloc>
+	typename vector<T, Alloc>::iterator
+		vector<T, Alloc>::insert(iterator position) {
+		size_type n = position - begin();
+		if (finish != end_of_storage && position == end()) {
+			construct(finish);
+			++finish;
+		}
+		else
+			insert_aux(position, T());
+		return begin() + n;
+	}
+
 	// 在 position 位置开始插入 n 个元素
 	template <class T, class Alloc>
 	void vector<T, Alloc>::insert(iterator position, size_type n, const T& x) {
+		fill_insert(position, n, x);
+	}
+
+	// 在 position 位置插入[first, last)内的元素
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::insert(iterator position,
+		InputIterator first, InputIterator last) {
+		typedef typename __is_integer<InputIterator>::is_integer integer;
+		insert_dispatch(position, first, last, integer());
+	}
+
+	// destroy_and_deallocate 函数
+	template <class T, class Alloc>
+	void vector<T, Alloc>::destroy_and_deallocate() {
+		destroy(start, finish);
+		data_allocator::deallocate(start, end_of_storage - start);
+	}
+
+	// allocate_and_fill 函数
+	template <class T, class Alloc>
+	void vector<T, Alloc>::allocate_and_fill(size_type n, const T& value) {
+		start = data_allocator::allocate(n);
+		finish = uninitialized_fill_n(start, n, value);
+		end_of_storage = finish;
+	}
+
+	// allocate_and_copy 函数
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::allocate_and_copy(InputIterator first, InputIterator last) {
+		difference_type n = last - first;
+		start = data_allocator::allocate(n);
+		finish = uninitialized_copy(first, last, start);
+		end_of_storage = finish;
+	}
+	
+	// fill_assign 函数
+	template <class T, class Alloc>
+	void vector<T, Alloc>::fill_assign(size_type n, const T& value) {
+		if (n > capacity()) {
+			vector<T, Alloc> tmp(n, value);
+			tmp.swap(*this);
+		}
+		else if (n > size()) {
+			fill(begin(), end(), value);
+			finish = uninitialized_fill_n(finish, n - size(), value);
+		}
+		else
+			earse(fill_n(start, n, value), finish);
+	}
+	
+	// assign_dispatch 函数
+	template <class T, class Alloc>
+	template <class Integer>
+	void vector<T, Alloc>::assign_dispatch(Integer n, Integer value, __true_type) {
+		fill_assign((size_type)n, (T)value);
+	}
+
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::assign_dispatch(InputIterator first, InputIterator last,
+		__false_type) {
+		assign_aux(first, last, iterator_category(first));
+	}
+
+	// assign_aux 函数
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::assign_aux(InputIterator first, InputIterator last,
+		input_iterator_tag) {
+		iterator cur = begin();
+		for (; first != last && cur != end(); ++first, ++cur) {
+			*cur = *first;
+		}
+		if (first == last)
+			earse(cur, end());
+		else
+			insert(end(), first, last);
+	}
+
+	template <class T, class Alloc>
+	template <class ForwardIterator>
+	void vector<T, Alloc>::assign_aux(ForwardIterator first, ForwardIterator last,
+		forward_iterator_tag) {
+		distance_type len = distance(first, last);
+		if (len > capacity()) {
+			destroy_and_deallocate();
+			allocate_and_copy(first, last);
+		}
+		else if (size() >= len) {
+			iterator new_finish = copy(first, last, start);
+			destroy(new_finish, finish);
+			finish = new_finish;
+		}
+		else {
+			ForwardIterator mid = first;
+			advance(mid, size());
+			copy(first, mid, start);
+			finish = uninitialized_copy(mid, last, finish);
+		}
+	}
+
+	// insert_aux 函数
+	template <class T, class Alloc>
+	void vector<T, Alloc>::insert_aux(iterator position, const T& x) {
+		if (finish != end_of_storage) {	//如果还有备用空间
+			construct(finish, *(finish - 1));	//在备用空间构造一个以vector最后一个元素为初值的元素
+			++finish;	//调整水位
+			copy_backward(position, finish - 2, finish - 1);	//position开始后移一位
+			T x_copy = x;
+			*position = x_copy;
+		}
+		else {
+			const size_type old_size = size();
+			// 如果原大小为0，则配置 1 个元素大小
+			// 如果原大小不为0，则配置两倍原来的大小
+			const size_type len = old_size != 0 ? 2 * old_size : 1;
+			iterator new_start = data_allocator::allocate(len);
+			iterator new_finish = new_start;
+			try {
+				//将原 vector 数据拷贝到新的 vector
+				new_finish = uninitialized_copy(start, position, new_start);
+				construct(new_finish, x);	//插入新元素
+				++new_finish;	//调整水位
+				new_finish = uninitialized_copy(position, finish, new_finish);	//拷贝剩余元素
+			}
+			catch (...) {
+				destroy(new_start, new_finish);
+				data_allocator::deallocate(new_start, len);
+				throw;
+			}
+			// 析构释放原 vector
+			destroy_and_deallocate();
+			// 调整迭代器，指向新的 vector
+			start = new_start;
+			finish = new_finish;
+			end_of_storage = new_start + len;
+		}
+	}
+	
+	// insert_dispatch 函数
+	template <class T, class Alloc>
+	template <class Integer>
+	void vector<T, Alloc>::insert_dispatch(iterator position, Integer n, Integer x,
+		__true_type) {
+		fill_insert(position, n, x);
+	}
+
+	template <class T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::insert_dispatch(iterator position, InputIterator first,
+		InputIterator last, __false_type) {
+		range_insert(position, first, last, iterator_category(first));
+	}
+
+	// fill_insert 函数
+	template <class T, class Alloc>
+	void vector<T, Alloc>::fill_insert(iterator position, size_type n, const T& x) {
 		if (n != 0) {
 			if (size_type(end_of_storage - finish) >= n) {
 				//备用空间大于等于增加的空间
@@ -276,7 +496,7 @@ namespace MyTinySTL {
 				}
 				catch (...) {
 					destroy(new_start, new_finish);
-					data_allocator::deallocate(new_start, new_finish);
+					data_allocator::deallocate(new_start, len);
 					throw;
 				}
 				// 析构释放原 vector
@@ -288,73 +508,74 @@ namespace MyTinySTL {
 			}
 		}
 	}
-
-	// destroy_and_deallocate 函数
+	
+	// range_insert 函数
 	template <class T, class Alloc>
-	void vector<T, Alloc>::destroy_and_deallocate() {
-		destroy(start, finish);
-		if (start)	data_allocator::deallocate(start, end_of_storage - start);
-	}
-
-	// allocate_and_fill 函数
-	template <class T, class Alloc>
-	void vector<T, Alloc>::allocate_and_fill(size_type n, const T& value) {
-		start = data_allocator::allocate(n);
-		finish = uninitialized_fill_n(start, n, value);
-		end_of_storage = finish;
-	}
-
-	// allocate_and_copy 函数
-	template <class T, class Alloc>
-	void vector<T, Alloc>::allocate_and_copy(iterator first, iterator last) {
-		difference_type n = last - first;
-		start = data_allocator::allocate(n);
-		finish = uninitialized_copy(first, last, start);
-		end_of_storage = finish;
-	}
-
-	// insert_aux 函数
-	template <class T, class Alloc>
-	void vector<T, Alloc>::insert_aux(iterator position, const T& x) {
-		if (position + 1 != end()) {	//如果还有备用空间
-			construct(finish, *(finish - 1));	//在备用空间构造一个以vector最后一个元素为初值的元素
-			++finish;	//调整水位
-			copy_backward(position, finish - 2, finish - 1);	//position开始后移一位
-			T x_copy = x;
-			*position = x_copy;
+	template <class InputIterator>
+	void vector<T, Alloc>::range_insert(iterator position, InputIterator first,
+		InputIterator last, input_iterator_tag) {
+		for (; first != last; ++first) {
+			position = insert(position, *first);
+			++position;
 		}
-		else {
-			const size_type old_size = size();
-			// 如果原大小为0，则配置 1 个元素大小
-			// 如果原大小不为0，则配置两倍原来的大小
-			const size_type len = old_size != 0 ? 2 * old_size : 1;
-			iterator new_start = data_allocator::allocate(len);
-			iterator new_finish = new_start;
-			try {
-				//将原 vector 数据拷贝到新的 vector
-				new_finish = uninitialized_copy(start, position, new_start);
-				construct(new_finish, x);	//插入新元素
-				++new_finish;	//调整水位
-				new_finish = uninitialized_copy(position, finish, new_finish);	//拷贝剩余元素
+	}
+
+	template <class T, class Alloc>
+	template <class ForwardIterator>
+	void vector<T, Alloc>::range_insert(iterator position, ForwardIterator first,
+		ForwardIterator last, forward_iterator_tag) {
+		if (first != last) {
+			size_type n = distance(first, last);
+			if (size_type(end_of_storage - finish) >= n) {
+				const size_type after_elems = finish - position;
+				iterator old_finish = finish;
+				if (after_elems > n) {
+					uninitialized_copy(finish - n, finish, finish);
+					finish += n;
+					copy_backward(position, old_finish - n, old_finish);
+					copy(first, last, position);
+				}
+				else {
+					ForwardIterator mid = first;
+					advance(mid, after_elems);
+					uninitialized_copy(mid, last, finish);
+					finish += n - after_elems;
+					uninitialized_copy(position, old_finish, finish);
+					finish += after_elems;
+					copy(first, mid, position);
+				}
 			}
-			catch (...) {
-				destroy(new_start, new_finish);
-				data_allocator::deallocate(new_start, len);
-				throw;
+			else {
+				//新长度：旧长度的两倍，旧长度+新增元素个数，取较大值
+				const size_type old_size = size();
+				const size_type len = old_size + max(old_size, n);
+				//配置新的 vector 空间
+				iterator new_start = data_allocator::allocate(len);
+				iterator new_finish = new_start;
+				try {
+					new_finish = uninitialized_copy(start, position, new_start);
+					new_finish = uninitialized_copy(first, last, new_finish);
+					new_finish = uninitialized_copy(position, finish, new_finish);
+				}
+				catch (...) {
+					destroy(new_start, new_finish);
+					data_allocator::deallocate(new_start, len);
+					throw;
+				}
+				// 析构释放原 vector
+				destroy_and_deallocate();
+				//调整迭代器，指向新的 vector
+				start = new_start;
+				finish = new_finish;
+				end_of_storage = start + len;
 			}
-			// 析构释放原 vector
-			destroy_and_deallocate();
-			// 调整迭代器，指向新的 vector
-			start = new_start;
-			finish = new_finish;
-			end_of_storage = new_start + len;
 		}
 	}
 	
 	// 重载比较操作符
 	template <class T, class Alloc>
 	inline bool operator==(const vector<T, Alloc>& x, const vector<T, Alloc>& y) {
-		return x.size() == y.size() && equal(x.begin(), x.end(), y.begin());
+		return x.size() == y.size() && equal(x.begin(), x.end(), y.begin(), y.end());
 	}
 
 	template <class T, class Alloc>
@@ -380,6 +601,12 @@ namespace MyTinySTL {
 	template <class T, class Alloc>
 	inline bool operator>=(const vector<T, Alloc>& x, const vector<T, Alloc>& y) {
 		return !(x < y);
+	}
+	
+	// 重载 MyTinySTL 的 swap
+	template <class T, class Alloc>
+	inline void swap(vector<T, Alloc>& x, vector<T, Alloc>& y) {
+		x.swap(y);
 	}
 }
 #endif // !VECTOR_H
