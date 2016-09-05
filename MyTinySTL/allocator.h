a928390d2f62044899841db49f324fa4f00fbc6c
Alinshans <13925588730@163.com>
2016-09-05 22:30:43 +0800

    Update allocator.h

--- MyTinySTL/allocator.h
+++ MyTinySTL/allocator.h
@@ -7,14 +7,14 @@
 namespace MyTinySTL {
 
 	//空间配置器
-	template <class T>
+	template <class T, class Alloc>
 	class allocator {
 
 	public:
-		typedef T			value_type;
-		typedef T*			pointer;
+		typedef T		value_type;
+		typedef T*		pointer;
 		typedef const T*	const_pointer;
-		typedef T&			reference;
+		typedef T&		reference;
 		typedef const T&	const_reference;
 		typedef size_t		size_type;
 		typedef ptrdiff_t	difference_type;
@@ -31,46 +31,46 @@ namespace MyTinySTL {
 		static void destroy(T* first, T* last);
 	};
 
-	template <class T>
-	T* allocator<T>::allocate() {
-		return static_cast<T*>(alloc::allocate(sizeof(T)));
+	template <class T, class Alloc>
+	T* allocator<T, Alloc>::allocate() {
+		return static_cast<T*>(Alloc::allocate(sizeof(T)));
 	}
 
-	template <class T>
-	T* allocator<T>::allocate(size_t n) {
+	template <class T, class Alloc>
+	T* allocator<T, Alloc>::allocate(size_t n) {
 		if (n == 0)	return 0;
-		return static_cast<T*>(alloc::allocate(n * sizeof(T)));
+		return static_cast<T*>(Alloc::allocate(n * sizeof(T)));
 	}
 
-	template <class T>
-	void allocator<T>::deallocate(T* ptr) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::deallocate(T* ptr) {
 		if (ptr == 0)	return;
-		alloc::deallocate(ptr, sizeof(T));
+		Alloc::deallocate(ptr, sizeof(T));
 	}
 
-	template <class T>
-	void allocator<T>::deallocate(T* ptr, size_t n) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::deallocate(T* ptr, size_t n) {
 		if (ptr == 0)	return;
-		alloc::deallocate(ptr, n * sizeof(T));
+		Alloc::deallocate(ptr, n * sizeof(T));
 	}
 
-	template <class T>
-	void allocator<T>::construct(T* ptr) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::construct(T* ptr) {
 		MyTinySTL::construct(ptr);
 	}
 
-	template <class T>
-	void allocator<T>::construct(T* ptr, const T& x) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::construct(T* ptr, const T& x) {
 		MyTinySTL::construct(ptr, x);
 	}
 
-	template <class T>
-	void allocator<T>::destroy(T* ptr) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::destroy(T* ptr) {
 		MyTinySTL::destroy(ptr);
 	}
 
-	template <class T>
-	void allocator<T>::destroy(T* first, T* last) {
+	template <class T, class Alloc>
+	void allocator<T, Alloc>::destroy(T* first, T* last) {
 		MyTinySTL::destroy(first, last);
 	}
 }
