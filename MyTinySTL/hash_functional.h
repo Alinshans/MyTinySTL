295a6cc489ddf8133e725f718c81b650d9b816c6
Alinshans <13925588730@163.com>
2016-09-25 21:57:27 +0800

    Create hash_functional.h

---
+++ MyTinySTL/hash_functional.h
@@ -0,0 +1,67 @@
+#ifndef HASH_FUNCTIONAL_H
+#define HASH_FUNCTIONAL_H
+
+#include <cstddef>
+
+namespace MyTinySTL {
+
+	// 对于大部分，hash 什么都不做
+	template <class Key>
+	struct hash {};
+
+	// 针对 const char * 设计的函数
+	inline size_t __hash_string(const char* s) {
+		unsigned long h = 0;
+		for (; *s; ++s)
+			h = 5 * h + *s;
+		return size_t(h);
+	}
+
+	template<> struct hash<char*> {
+		size_t operator()(const char* s) const { return __hash_string(s); }
+	};
+
+	template<> struct hash<const char*> {
+		size_t operator()(const char* s) const { return __hash_string(s); }
+	};
+
+	// 针对 char，int，long等整数型别，只是返回原值
+
+	template <> struct hash<char> {
+		size_t operator()(char x) const { return x; }
+	};
+
+	template <> struct hash<unsigned char> {
+		size_t operator()(unsigned char x) const { return x; }
+	};
+
+	template <> struct hash<signed char> {
+		size_t operator()(unsigned char x) const { return x; }
+	};
+
+	template <> struct hash<short> {
+		size_t operator()(short x) const { return x; }
+	};
+
+	template <> struct hash<unsigned short> {
+		size_t operator()(unsigned short x) const { return x; }
+	};
+
+	template <> struct hash<int> {
+		size_t operator()(int x) const { return x; }
+	};
+
+	template <> struct hash<unsigned int> {
+		size_t operator()(unsigned int x) const { return x; }
+	};
+
+	template <> struct hash<long> {
+		size_t operator()(long x) const { return x; }
+	};
+
+	template <> struct hash<unsigned long> {
+		size_t operator()(unsigned long x) const { return x; }
+	};
+}
+#endif // !HASH_FUNCTIONAL_H
+
