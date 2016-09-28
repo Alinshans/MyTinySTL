090b588002d5fe861cd40c20219cd8b235a76ab3
刘俊延 <13925588730@163.com>
2016-09-28 17:04:44 +0800

    Update construct.h

--- MyTinySTL/construct.h
+++ MyTinySTL/construct.h
@@ -1,18 +1,20 @@
-#ifndef CONSTRUCT_H
-#define CONSTRUCT_H
+#ifndef MYTINYSTL_CONSTRUCT_H_
+#define MYTINYSTL_CONSTRUCT_H_
 
-#ifndef USE_NEW
-#define	USE_NEW
+// 这个头文件包含两个全局函数 construct, destroy
+// construct 负责对象的构造，destroy 负责对象的析构
+
 #include <new>
-#endif // !USE_NEW
 
 #include "type_traits.h"
 #include "iterator.h"
 
-//两个全局函数： construct()用于构造 和 destroy()用于析构
 namespace MyTinySTL {
 	
-	/********************** construct *********************/
+	// construct
+	// 构造对象的内容
+	// 版本1：接受一个指针
+	// 版本2：接受一个指针和初始化值
 	template <class T1>
 	inline void construct(T1 *p) {
 		new ((void*)p) T1();
@@ -23,7 +25,10 @@ namespace MyTinySTL {
 		new ((void*)p) T1(value);
 	}
 
-	/*********************** destroy **********************/
+	// destroy
+	// 将对象析构
+	// 版本1：接受一个指针
+	// 版本2：接受两个迭代器，根据元素型别，采用最适合的方法析构[first,last)区间的对象
 	template <class T>
 	inline void destroy(T * pointer) {
 		pointer->~T();
@@ -36,8 +41,8 @@ namespace MyTinySTL {
 
 	template <class ForwardIterator, class T>
 	inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
-		typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
-		return __destroy_aux(first, last, trivial_destructor());
+		typedef typename __type_traits<T>::has_trivial_destructor TrivialDestructor;
+		return __destroy_aux(first, last, TrivialDestructor());
 	}
 
 	template <class ForwardIterator>
@@ -46,15 +51,13 @@ namespace MyTinySTL {
 			destroy(&*first);
 	}
 
+	// 如果是 __true_type，则什么也不做
 	template <class ForwardIterator>
 	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
 
-	//针对char* 和 wchar_t* 的特化版本
-	template <>
-	inline void destroy(char*, char*) {}
+	//针对 char* 和 wchar_t* 的特化版本
+	template <> inline void destroy(char*, char*) {}
 
-	template <>
-	inline void destroy(wchar_t*, wchar_t*) {}
+	template <> inline void destroy(wchar_t*, wchar_t*) {}
 }
-#endif // !CONSTRUCT_H
-
+#endif // !MYTINYSTL_CONSTRUCT_H_
