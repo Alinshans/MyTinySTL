b54945db388ef9f763f6510767e0c6cd9b5c8232
刘俊延 <13925588730@163.com>
2016-09-29 19:54:14 +0800

    Update pair.h

--- MyTinySTL/pair.h
+++ MyTinySTL/pair.h
@@ -1,16 +1,24 @@
-#ifndef PAIR_H
-#define PAIR_H
+#ifndef MYTINYSTL_PAIR_H_
+#define MYTINYSTL_PAIR_H_
+
+// 这个头文件包含一个模板结构体 pair，代表一种数据类型
 
 namespace MyTinySTL {
 
+	// 结构体模板 : pair
+	// 有两个模板参数，参数1是第一种数据类型，参数2是第二种数据类型
+	// 用 first 和 second 来分别取出第一个与第二个数据
+	// 例： pair<const char*, int> p("Jun",31);
+	// cout << p.first << " " << p.second << endl;	// Jun 31
 	template <class T1, class T2>
 	struct pair {
 		typedef T1	first_type;
 		typedef T2	second_type;
 
-		T1 first;
-		T2 second;
+		T1 first;	// 保存第一个数据
+		T2 second;	// 保存第二个数据
 
+		// 构造函数
 		pair() :first(T1()), second(T2()) {}
 		pair(const T1& a, const T2& b) :first(a), second(b) {}
 
@@ -18,38 +26,41 @@ namespace MyTinySTL {
 		pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
 	};
 
+	// 重载比较操作符 
 	template <class T1, class T2>
-	inline bool operator== (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return x.first == y.first && x.second == y.second;
 	}
 
 	template <class T1, class T2>
-	inline bool operator< (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
 	}
+
 	template <class T1, class T2>
-	inline bool operator!= (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator!=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return !(x == y);
 	}
 
 	template <class T1, class T2>
-	inline bool operator> (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator>(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return y < x;
 	}
 
 	template <class T1, class T2>
-	inline bool operator<= (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator<=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return !(y < x);
 	}
 
 	template <class T1, class T2>
-	inline bool operator>= (const pair<T1, T2>& x, const pair<T1, T2>& y) {
+	inline bool operator>=(const pair<T1, T2>& x, const pair<T1, T2>& y) {
 		return !(x < y);
 	}
 
+	// 一个全局函数，使两个数据成为一个 pair
 	template <class T1, class T2>
 	inline pair<T1, T2> make_pair(const T1& x, const T2& y) {
 		return pair<T1, T2>(x, y);
 	}
 }
-#endif // !PAIR_H
+#endif // !MYTINYSTL_PAIR_H_
