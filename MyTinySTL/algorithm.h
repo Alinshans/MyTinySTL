b57d33a6513342439eae83bf7168b55f8b08d7b3
刘俊延 <13925588730@163.com>
2016-10-02 15:54:20 +0800

    Update algorithm.h

--- MyTinySTL/algorithm.h
+++ MyTinySTL/algorithm.h
@@ -1,10 +1,13 @@
-#ifndef ALGORITHM_H
-#define ALGORITHM_H
+#ifndef MYTINYSTL_ALGORITHM_H_
+#define MYTINYSTL_ALGORITHM_H_
+
+// 这个头文件包含了 MyTinySTL 的所有算法，包括基础算法，数值算法，heap算法，set算法和其他算法
 
 #include "algo.h"
 #include "algobase.h"
-#include "numeric.h"
 #include "heap_algo.h"
+#include "numeric.h"
 #include "set_algo.h"
 
-#endif // !ALGORITHM_H
+#endif // !MYTINYSTL_ALGORITHM_H_
+
