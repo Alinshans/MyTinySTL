f548f0c7a3c5daa5bbf79c8a2ddd39721751c7bb
刘俊延 <13925588730@163.com>
2016-10-03 09:48:42 +0800

    规范格式

--- MyTinySTL/functional.h
+++ MyTinySTL/functional.h
@@ -1,7 +1,9 @@
-#ifndef FUNCTIONAL_H
-#define FUNCTIONAL_H
+#ifndef MYTINYSTL_FUNCTIONAL_H_
+#define MYTINYSTL_FUNCTIONAL_H_
 
-namespace MyTinySTL {
+// 这个头文件包含了 MyTinySTL 的函数对象(即仿函数)
+
+namespace mystl {
 
 	// 定义一元函数的参数型别和返回值型别
 	template <class Arg, class Result>
@@ -18,37 +20,37 @@ namespace MyTinySTL {
 		typedef Result	result_type;
 	};
 
-	// 仿函数：加法
+	// 函数对象：加法
 	template <class T>
 	struct plus :public binary_function<T, T, T> {
 		T operator()(const T& x, const T& y) const { return x + y; }
 	};
 
-	// 仿函数：减法
+	// 函数对象：减法
 	template <class T>
 	struct minus :public binary_function<T, T, T> {
 		T operator()(const T& x, const T& y) const { return x - y; }
 	};
 
-	// 仿函数：乘法
+	// 函数对象：乘法
 	template <class T>
 	struct multiplies :public binary_function<T, T, T> {
 		T operator()(const T& x, const T& y) const { return x * y; }
 	};
 
-	// 仿函数：除法
+	// 函数对象：除法
 	template <class T>
 	struct divides :public binary_function<T, T, T> {
 		T operator()(const T& x, const T& y) const { return x / y; }
 	};
 
-	// 仿函数：模取
+	// 函数对象：模取
 	template <class T>
 	struct modulus :public binary_function<T, T, T> {
 		T operator()(const T& x, const T& y) const { return x % y; }
 	};
 
-	// 仿函数：否定
+	// 函数对象：否定
 	template <class T>
 	struct negate :public unarg_function<T, T> {
 		T operator()(const T& x) const { return -x; }
@@ -62,55 +64,55 @@ namespace MyTinySTL {
 	template <class T>
 	inline T identity_element(multiplies<T>) { return T(1); }
 
-	// 仿函数：等于
+	// 函数对象：等于
 	template <class T>
 	struct equal_to :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x == y; }
 	};
 
-	// 仿函数：不等于
+	// 函数对象：不等于
 	template <class T>
 	struct not_equal_to :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x != y; }
 	};
 
-	// 仿函数：大于
+	// 函数对象：大于
 	template <class T>
 	struct greater :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x > y; }
 	};
 
-	// 仿函数：小于
+	// 函数对象：小于
 	template <class T>
 	struct less :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x < y; }
 	};
 
-	// 仿函数：大于等于
+	// 函数对象：大于等于
 	template <class T>
 	struct greater_equal :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x >= y; }
 	};
 
-	// 仿函数：小于等于
+	// 函数对象：小于等于
 	template <class T>
 	struct less_equal :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x <= y; }
 	};
 
-	// 仿函数：逻辑与
+	// 函数对象：逻辑与
 	template <class T>
 	struct logical_and :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x && y; }
 	};
 
-	// 仿函数：逻辑或
+	// 函数对象：逻辑或
 	template <class T>
 	struct logical_or :public binary_function<T, T, bool> {
 		bool operator()(const T& x, const T& y) const { return x || y; }
 	};
 
-	// 仿函数：逻辑非
+	// 函数对象：逻辑非
 	template <class T>
 	struct logical_not :public unarg_function<T, bool> {
 		bool operator()(const T& x) const { return !x; }
@@ -150,4 +152,4 @@ namespace MyTinySTL {
 		Arg2 operator()(const Arg1&, const Arg2& y) const { return y; }
 	};
 }
-#endif // !FUNCTIONAL_H
+#endif // !MYTINYSTL_FUNCTIONAL_H_
