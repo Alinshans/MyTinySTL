9161e3136427bfad38c67b71229c928eca7c7c0f
刘俊延 <13925588730@163.com>
2016-10-04 15:54:28 +0800

    规范格式

--- MyTinySTL/algorithm.h
+++ MyTinySTL/algorithm.h
@@ -1,7 +1,7 @@
 #ifndef MYTINYSTL_ALGORITHM_H_
 #define MYTINYSTL_ALGORITHM_H_
 
-// 这个头文件包含了 MyTinySTL 的所有算法，包括基础算法，数值算法，heap算法，set算法和其他算法
+// 这个头文件包含了 mystl 的所有算法，包括基础算法，数值算法，heap算法，set算法和其他算法
 
 #include "algo.h"
 #include "algobase.h"
@@ -10,3 +10,4 @@
 #include "set_algo.h"
 
 #endif // !MYTINYSTL_ALGORITHM_H_
+
