76b94e0a9c2777e4f9da7588c9940741e991eac4
刘俊延 <13925588730@163.com>
2016-10-16 00:43:29 +0800

    修改 inner_product  的参数

--- MyTinySTL/numeric.h
+++ MyTinySTL/numeric.h
@@ -69,8 +69,7 @@ namespace mystl {
 	// 版本2：以仿函数提供 + 和 * 的操作
 	/*********************************************************************************/
 	template <class InputIterator1, class InputIterator2, class T>
-	T inner_product(InputIterator1 first1, InputIterator1 last1,
-		InputIterator2 first2, InputIterator2 last2, T init) {
+	T inner_product(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, T init) {
 		for (; first1 != last1; ++first1, ++first2) {
 			init = init + (*first1 * *first2);
 		}
@@ -81,7 +80,7 @@ namespace mystl {
 	template <class InputIterator1, class InputIterator2, class T,
 		class BinaryOperation1, class BinaryOperation2>
 	T inner_product(InputIterator1 first1, InputIterator1 last1,InputIterator2 first2,
-		InputIterator2 last2, T init, BinaryOperation1 binary_op1, BinaryOperation2 binary_op2) {
+		T init, BinaryOperation1 binary_op1, BinaryOperation2 binary_op2) {
 		for (; first1 != last1; ++first1, ++first2) {
 			init = binary_op1(init, binary_op2(*first1, *first2));
 		}
