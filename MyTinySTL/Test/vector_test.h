ca08b2fd943b2093965d95a94b72bf84a49162be
刘俊延 <13925588730@163.com>
2016-10-20 21:02:55 +0800

    完成 vector 测试

--- MyTinySTL/Test/vector_test.h
+++ MyTinySTL/Test/vector_test.h
@@ -16,12 +16,12 @@ namespace mystl {
 	namespace test {
 		namespace vector_test {
 			
-			// push_back 的性能测试
-			#define PUSH_BACK_TEST(mode, count) do { \
+			// vector push_back 的性能测试
+			#define VECTOR_PUSH_BACK_TEST(mode, count) do { \
 				srand((int)time(0)); \
 				clock_t start, end; \
-				char buf[5]; \
 				mode::vector<int> v; \
+				char buf[5]; \
 				start = clock(); \
 				for (size_t i = 0; i < count; ++i) \
 					v.push_back(rand()); \
@@ -29,8 +29,8 @@ namespace mystl {
 				int n = end - start; \
 				sprintf_s(buf, "%d", n); \
 				string t = buf; \
-				t += "ms   /"; \
-				cout << setw(12) << t; \
+				t += "ms    |"; \
+				cout << setw(14) << t; \
 			} while(0)
 			
 
@@ -48,92 +48,64 @@ namespace mystl {
 				mystl::vector<int> v7 = v3;
 				mystl::vector<int> v8 = std::move(v3);
 				v8.~vector();
-			
-				cout << " v1 assign(10):" << endl;
-				v1.assign(10);
-				COUT(v1);
-				cout << " v1 assign(8,8):" << endl;
-				v1.assign(8, 8);
-				COUT(v1);
-				cout << " v1 assign(a,a+5):" << endl;
-				v1.assign(a, a + 5);
-				COUT(v1);
-				cout << " v1 push_back(6):" << endl;
-				v1.push_back(6);
-				COUT(v1);
-				cout << " v1 insert(v1.begin()):" << endl;
-				v1.insert(v1.begin());
-				COUT(v1);
-				cout << " v1 insert(v1.end(),7):" << endl;
-				v1.insert(v1.end(), 7);
-				COUT(v1);
-				cout << " v1 insert(v1.begin()+3,2,3):" << endl;
-				v1.insert(v1.begin() + 3, 2, 3);
-				COUT(v1);
-				cout << " v1 insert(v1.begin(),a,a+5):" << endl;
-				v1.insert(v1.begin(), a, a + 5);
-				COUT(v1);
-				cout << " v1 pop_back():" << endl;
-				v1.pop_back();
-				COUT(v1);
-				cout << " v1 erase(v1.begin()):" << endl;
-				v1.erase(v1.begin());
-				COUT(v1);
-				cout << " v1 erase(v1.begin(),v1.begin()+2):" << endl;
-				v1.erase(v1.begin(), v1.begin() + 2);
-				COUT(v1);
-				cout << " v1 reverse():" << endl;
-				v1.reverse();
-				COUT(v1);
-				cout << " v1 swap(v4):" << endl;
-				v1.swap(v4);
-				COUT(v1);
-				cout << " *v1.begin():" << *v1.begin() << endl;
-				cout << " *(v1.end()-1):" << *(v1.end() - 1) << endl;
-				cout << " *v1.rbegin():" << *v1.rbegin() << endl;
-				cout << " *(v1.rend()-1):" << *(v1.rend() - 1) << endl;
-				cout << " v1.front():" << v1.front() << endl;
-				cout << " v1.back():" << v1.back() << endl;
-				cout << " v1[0]:" << v1[0] << endl;
-				cout << " v1.at(1):" << v1.at(1) << endl;
-				cout << " v1 empty():" << boolalpha << v1.empty() << noboolalpha << endl;
-				cout << " v1 size():" << v1.size() << endl;
-				cout << " v1 max_size():" << v1.max_size() << endl;
-				cout << " v1 capacity():" << v1.capacity() << endl;
-				cout << " v1 resize(10):" << endl;
-				v1.resize(10);
-				COUT(v1);
-				cout << " v1 size():" << v1.size() << " ,v1 capacity():" << v1.capacity() << endl;
-				cout << " v1 resize(6,6):" << endl;
-				v1.resize(6, 6);
-				COUT(v1);
-				cout << " v1 size():" << v1.size() << " ,v1 capacity():" << v1.capacity() << endl;
-				cout << " v1 clear():" << endl;
-				v1.clear();
-				cout << " v1 size():" << v1.size() << " ,v1 capacity():" << v1.capacity() << endl;
-				cout << " v1 reseve(5):" << endl;
-				v1.reserve(5);
-				cout << " v1 size():" << v1.size() << " ,v1 capacity():" << v1.capacity() << endl;
-				cout << " v1 reserve(20):" << endl;
-				v1.reserve(20);
-				cout << " v1 size():" << v1.size() << " ,v1 capacity():" << v1.capacity() << endl;
-				cout << "[ PASSED ]" << endl;
+				
+				FUN_AFTER(v1, v1.assign(10));
+				FUN_AFTER(v1, v1.assign(8, 8));
+				FUN_AFTER(v1, v1.assign(a, a + 5));
+				FUN_AFTER(v1, v1.push_back(6));
+				FUN_AFTER(v1, v1.insert(v1.begin()));
+				FUN_AFTER(v1, v1.insert(v1.end(), 7));
+				FUN_AFTER(v1, v1.insert(v1.begin() + 3, 2, 3));
+				FUN_AFTER(v1, v1.insert(v1.begin(), a, a + 5));
+				FUN_AFTER(v1, v1.pop_back());
+				FUN_AFTER(v1, v1.erase(v1.begin()));
+				FUN_AFTER(v1, v1.erase(v1.begin(), v1.begin() + 2));
+				FUN_AFTER(v1, v1.reverse());
+				FUN_AFTER(v1, v1.swap(v4));
+				FUN_VALUE(*v1.begin());
+				FUN_VALUE(*(v1.end() - 1));
+				FUN_VALUE(*v1.rbegin());
+				FUN_VALUE(*(v1.rend() - 1));
+				FUN_VALUE(v1.front());
+				FUN_VALUE(v1.back());
+				FUN_VALUE(v1[0]);
+				FUN_VALUE(v1.at(1));
+				cout << boolalpha;
+				FUN_VALUE(v1.empty());
+				cout << noboolalpha;
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.max_size());
+				FUN_VALUE(v1.capacity());
+				FUN_AFTER(v1, v1.resize(10));
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.capacity());
+				FUN_AFTER(v1, v1.resize(6, 6));
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.capacity());
+				FUN_AFTER(v1, v1.clear());
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.capacity());
+				FUN_AFTER(v1, v1.reserve(5));
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.capacity());
+				FUN_AFTER(v1, v1.reserve(20));
+				FUN_VALUE(v1.size());
+				FUN_VALUE(v1.capacity());
+				PASSED;
 				cout << "[------------------ Performance Testing ------------------]" << endl;
-				mystl::vector<int> mv;
-				std::vector<int> sv;
-				cout << "/-----------/-----------/-----------/-----------/" << endl;
-				cout << "/ push_back /  1000000  / 10000000  / 100000000 /" << endl;
-				cout << "/   std     /";
-				PUSH_BACK_TEST(std, 1000000);
-				PUSH_BACK_TEST(std, 10000000);
-				PUSH_BACK_TEST(std, 100000000);
-				cout << endl << "/   mystl   /";
-				PUSH_BACK_TEST(mystl, 1000000);
-				PUSH_BACK_TEST(mystl, 10000000);
-				PUSH_BACK_TEST(mystl, 100000000);
+				cout << "|---------------|-------------|-------------|-------------|" << endl;
+				cout << "|   push_back   |   1000000   |  10000000   |  100000000  |" << endl;
+				cout << "|      std      |";
+				VECTOR_PUSH_BACK_TEST(std, 1000000);
+				VECTOR_PUSH_BACK_TEST(std, 10000000);
+				VECTOR_PUSH_BACK_TEST(std, 100000000);
+				cout << endl << "|     mystl     |";
+				VECTOR_PUSH_BACK_TEST(mystl, 1000000);
+				VECTOR_PUSH_BACK_TEST(mystl, 10000000);
+				VECTOR_PUSH_BACK_TEST(mystl, 100000000);
 				cout << endl;
-				cout << "/-----------/-----------/-----------/-----------/" << endl;
-				cout << "[ PASSED ]" << endl;
+				cout << "|---------------|-------------|-------------|-------------|" << endl;
+				PASSED;
 				cout << "[-------------- End container test : vector --------------]" << endl;
 			}
 		}
