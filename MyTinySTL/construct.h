c07a93e97e4ffeee214409d4f6a0d147cadada7b
刘俊延 <13925588730@163.com>
2016-10-21 20:49:16 +0800

    增加 destroy 对指针的特化

--- MyTinySTL/construct.h
+++ MyTinySTL/construct.h
@@ -1,7 +1,7 @@
 #ifndef MYTINYSTL_CONSTRUCT_H_
 #define MYTINYSTL_CONSTRUCT_H_
 
-// 这个头文件包含两个全局函数 construct, destroy
+// 这个头文件包含两个全局函数 construct，destroy
 // construct 负责对象的构造，destroy 负责对象的析构
 
 #include <new>
@@ -9,7 +9,7 @@
 #include "type_traits.h"
 #include "iterator.h"
 
-namespace MyTinySTL {
+namespace mystl {
 	
 	// construct
 	// 构造对象的内容
@@ -55,10 +55,12 @@ namespace MyTinySTL {
 	template <class ForwardIterator>
 	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
 
-	//针对 char* 和 wchar_t* 的特化版本
-	template <> inline void destroy(char*, char*) {}
-
-	template <> inline void destroy(wchar_t*, wchar_t*) {}
+	// 如果销毁的对象是指针，则什么也不做
+	inline void destroy(char*, char*) {}
+	inline void destroy(wchar_t*, wchar_t*) {}
+	inline void destroy(int*, int*) {}
+	inline void destroy(long*, long*) {}
+	inline void destroy(float*, float*) {}
+	inline void destroy(double*, double*) {}
 }
 #endif // !MYTINYSTL_CONSTRUCT_H_
-
