ffcfcb1b52405dda81aab97836dee6b87893d220
刘俊延 <13925588730@163.com>
2016-10-24 13:07:51 +0800

    完成 queue 测试

--- MyTinySTL/Test/queue_test.h
+++ MyTinySTL/Test/queue_test.h
@@ -1,7 +1,7 @@
-#ifndef MYTINYSTL_QUEUE_TEST_H
-#define MYTINYSTL_QUEUE_TEST_H
+#ifndef MYTINYSTL_QUEUE_TEST_H_
+#define MYTINYSTL_QUEUE_TEST_H_
 
-// queue 测试: 测试 queue，priority_queue 的 API 和它们 push 的性能
+// queue test : 测试 queue,priority_queue 的 API 和它们 push 的性能
 
 #include <iomanip>
 #include <iostream>
@@ -12,8 +12,6 @@
 #include "..\vector.h"
 #include "test.h"
 
-using namespace std;
-
 namespace mystl {
 	namespace test {
 		namespace queue_test {
@@ -21,34 +19,34 @@ namespace mystl {
 			// 遍历输出 queue 的宏定义
 			#define QUEUE_COUT(q) do { \
 				std::string q_name = #q; \
-				cout << " " << q_name << " :"; \
+				std::cout << " " << q_name << " :"; \
 				queue_print(q); \
 			} while(0)
 
 			// 遍历输出 priority_queue 的宏定义
 			#define P_QUEUE_COUT(p) do { \
 				std::string p_name = #p; \
-				cout << " " << p_name << " :"; \
+				std::cout << " " << p_name << " :"; \
 				p_queue_print(p); \
 			} while(0)
 
 			void queue_print(mystl::queue<int> q) {
 				while (!q.empty()) {
-					cout << " " << q.front();
+					std::cout << " " << q.front();
 					q.pop();
 				}
-				cout << endl;
+				std::cout << std::endl;
 			}
 
 			void p_queue_print(mystl::priority_queue<int> p) {
 				while (!p.empty()) {
-					cout << " " << p.top();
+					std::cout << " " << p.top();
 					p.pop();
 				}
-				cout << endl;
+				std::cout << std::endl;
 			}
 
-			// queue，priority_queue push 的性能测试
+			// queue,priority_queue push 的性能测试
 			#define QUEUE_PUSH_TEST(mode, con, count) do { \
 				srand((int)time(0)); \
 				clock_t start, end; \
@@ -60,15 +58,15 @@ namespace mystl {
 				end = clock(); \
 				int n = end - start; \
 				sprintf_s(buf, "%d", n); \
-				string t = buf; \
+				std::string t = buf; \
 				t += "ms    |"; \
-				cout << setw(14) << t; \
+				std::cout << std::setw(14) << t; \
 			} while(0)
 
 			void queue_test() {
-				cout << "[=========================================================]" << endl;
-				cout << "[-------------- Run container test : queue ---------------]" << endl;
-				cout << "[----------------------- API test ------------------------]" << endl;
+				std::cout << "[=========================================================]" << std::endl;
+				std::cout << "[-------------- Run container test : queue ---------------]" << std::endl;
+				std::cout << "[----------------------- API test ------------------------]" << std::endl;
 				int a[] = { 1,2,3,4,5 };
 				mystl::deque<int> d1(5);
 				mystl::queue<int> q1;
@@ -82,52 +80,52 @@ namespace mystl {
 				mystl::queue<int> q9 = q3;
 				mystl::queue<int> q10 = std::move(q3);
 
-				cout << " After q1 push 1,2,3,4,5 :" << endl;
+				std::cout << " After q1 push 1,2,3,4,5 :" << std::endl;
 				q1.push(1);
 				q1.push(2);
 				q1.push(3);
 				q1.push(4);
 				q1.push(5);
 				QUEUE_COUT(q1);
-				cout << boolalpha;
+				std::cout << std::boolalpha;
 				FUN_VALUE(q1.empty());
-				cout << noboolalpha;
+				std::cout << std::noboolalpha;
 				FUN_VALUE(q1.size());
 				FUN_VALUE(q1.front());
 				FUN_VALUE(q1.back());
 				while (!q1.empty()) {
-					cout << " After q1.pop():" << endl;
+					std::cout << " After q1.pop():" << std::endl;
 					q1.pop();
 					QUEUE_COUT(q1);
 				}
-				cout << " After q1.swap(q4) :" << endl;
+				std::cout << " After q1.swap(q4) :" << std::endl;
 				q1.swap(q4);
 				QUEUE_COUT(q1);
-				cout << " After q1.clear() :" << endl;
+				std::cout << " After q1.clear() :" << std::endl;
 				q1.clear();
 				QUEUE_COUT(q1);
 				PASSED;
-				cout << "[------------------ Performance Testing ------------------]" << endl;
-				cout << "|---------------|-------------|-------------|-------------|" << endl;
-				cout << "|     push      |   1000000   |  10000000   |  100000000  |" << endl;
-				cout << "|      std      |";
+				std::cout << "[------------------ Performance Testing ------------------]" << std::endl;
+				std::cout << "|---------------|-------------|-------------|-------------|" << std::endl;
+				std::cout << "|     push      |   1000000   |  10000000   |  100000000  |" << std::endl;
+				std::cout << "|      std      |";
 				QUEUE_PUSH_TEST(std, queue, 1000000);
 				QUEUE_PUSH_TEST(std, queue, 10000000);
 				QUEUE_PUSH_TEST(std, queue, 100000000);
-				cout << endl << "|     mystl     |";
+				std::cout << std::endl << "|     mystl     |";
 				QUEUE_PUSH_TEST(mystl, queue, 1000000);
 				QUEUE_PUSH_TEST(mystl, queue, 10000000);
 				QUEUE_PUSH_TEST(mystl, queue, 100000000);
-				cout << endl;
-				cout << "|---------------|-------------|-------------|-------------|" << endl;
+				std::cout << std::endl;
+				std::cout << "|---------------|-------------|-------------|-------------|" << std::endl;
 				PASSED;
-				cout << "[-------------- End container test : queue ---------------]" << endl;
+				std::cout << "[-------------- End container test : queue ---------------]" << std::endl;
 			}
 
 			void priority_test() {
-				cout << "[=========================================================]" << endl;
-				cout << "[---------- Run container test : priority_queue ----------]" << endl;
-				cout << "[----------------------- API test ------------------------]" << endl;
+				std::cout << "[=========================================================]" << std::endl;
+				std::cout << "[---------- Run container test : priority_queue ----------]" << std::endl;
+				std::cout << "[----------------------- API test ------------------------]" << std::endl;
 				int a[] = { 1,2,3,4,5 };
 				mystl::vector<int> v1(5);
 				mystl::priority_queue<int> p1;
@@ -141,48 +139,48 @@ namespace mystl {
 				mystl::priority_queue<int> p9 = p3;
 				mystl::priority_queue<int> p10 = std::move(p3);
 
-				cout << " After p1 push 1,2,3,4,5 :" << endl;
+				std::cout << " After p1 push 1,2,3,4,5 :" << std::endl;
 				p1.push(1);
 				p1.push(2);
 				p1.push(3);
 				p1.push(4);
 				p1.push(5);
 				P_QUEUE_COUT(p1);
-				cout << boolalpha;
+				std::cout << std::boolalpha;
 				FUN_VALUE(p1.empty());
-				cout << noboolalpha;
+				std::cout << std::noboolalpha;
 				FUN_VALUE(p1.size());
 				FUN_VALUE(p1.top());
 				while (!p1.empty()) {
-					cout << " After p1.pop():" << endl;
+					std::cout << " After p1.pop():" << std::endl;
 					p1.pop();
 					P_QUEUE_COUT(p1);
 				}
-				cout << " After p1.swap(p4) :" << endl;
+				std::cout << " After p1.swap(p4) :" << std::endl;
 				p1.swap(p4);
 				P_QUEUE_COUT(p1);
-				cout << " After p1.clear() :" << endl;
+				std::cout << " After p1.clear() :" << std::endl;
 				p1.clear();
 				P_QUEUE_COUT(p1);
 				PASSED;
-				cout << "[------------------ Performance Testing ------------------]" << endl;
-				cout << "|---------------|-------------|-------------|-------------|" << endl;
-				cout << "|     push      |   1000000   |  10000000   |  100000000  |" << endl;
-				cout << "|      std      |";
+				std::cout << "[------------------ Performance Testing ------------------]" << std::endl;
+				std::cout << "|---------------|-------------|-------------|-------------|" << std::endl;
+				std::cout << "|     push      |   1000000   |  10000000   |  100000000  |" << std::endl;
+				std::cout << "|      std      |";
 				QUEUE_PUSH_TEST(std, priority_queue, 1000000);
 				QUEUE_PUSH_TEST(std, priority_queue, 10000000);
 				QUEUE_PUSH_TEST(std, priority_queue, 100000000);
-				cout << endl << "|     mystl     |";
+				std::cout << std::endl << "|     mystl     |";
 				QUEUE_PUSH_TEST(mystl, priority_queue, 1000000);
 				QUEUE_PUSH_TEST(mystl, priority_queue, 10000000);
 				QUEUE_PUSH_TEST(mystl, priority_queue, 100000000);
-				cout << endl;
-				cout << "|---------------|-------------|-------------|-------------|" << endl;
+				std::cout << std::endl;
+				std::cout << "|---------------|-------------|-------------|-------------|" << std::endl;
 				PASSED;
-				cout << "[---------- Run container test : priority_queue ----------]" << endl;
+				std::cout << "[---------- Run container test : priority_queue ----------]" << std::endl;
 			}
 		}
 	}
 }
-#endif // !MYTINYSTL_QUEUE_TEST_H
+#endif // !MYTINYSTL_QUEUE_TEST_H_
 
