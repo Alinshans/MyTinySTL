ddf81bf258f0095d928a282ddd027fc12d11679d
刘俊延 <13925588730@163.com>
2016-11-21 21:28:08 +0800

    clean up

--- MyTinySTL/construct.h
+++ MyTinySTL/construct.h
@@ -10,57 +10,57 @@
 #include "iterator.h"
 
 namespace mystl {
-	
-	// construct
-	// 构造对象的内容
-	// 版本1：接受一个指针
-	// 版本2：接受一个指针和初始化值
-	template <class T1>
-	inline void construct(T1 *p) {
-		new ((void*)p) T1();
-	}
+    
+    // construct
+    // 构造对象的内容
+    // 版本1：接受一个指针
+    // 版本2：接受一个指针和初始化值
+    template <class T1>
+    inline void construct(T1 *p) {
+        new ((void*)p) T1();
+    }
 
-	template <class T1, class T2>
-	inline void construct(T1 *p, const T2 &value) {
-		new ((void*)p) T1(value);
-	}
+    template <class T1, class T2>
+    inline void construct(T1 *p, const T2 &value) {
+        new ((void*)p) T1(value);
+    }
 
-	// destroy
-	// 将对象析构
-	// 版本1：接受一个指针
-	// 版本2：接受两个迭代器，根据元素型别，采用最适合的方法析构[first,last)区间的对象
-	template <class T>
-	inline void destroy(T * pointer) {
-		pointer->~T();
-	}
+    // destroy
+    // 将对象析构
+    // 版本1：接受一个指针
+    // 版本2：接受两个迭代器，根据元素型别，采用最适合的方法析构[first,last)区间的对象
+    template <class T>
+    inline void destroy(T * pointer) {
+        pointer->~T();
+    }
 
-	template <class ForwardIterator>
-	inline void destroy(ForwardIterator first, ForwardIterator last) {
-		__destroy(first, last, value_type(first));
-	}
+    template <class ForwardIterator>
+    inline void destroy(ForwardIterator first, ForwardIterator last) {
+        __destroy(first, last, value_type(first));
+    }
 
-	template <class ForwardIterator, class T>
-	inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
-		typedef typename __type_traits<T>::has_trivial_destructor TrivialDestructor;
-		return __destroy_aux(first, last, TrivialDestructor());
-	}
+    template <class ForwardIterator, class T>
+    inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
+        typedef typename __type_traits<T>::has_trivial_destructor TrivialDestructor;
+        return __destroy_aux(first, last, TrivialDestructor());
+    }
 
-	template <class ForwardIterator>
-	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type) {
-		for (; first != last; ++first)
-			destroy(&*first);
-	}
+    template <class ForwardIterator>
+    inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type) {
+        for (; first != last; ++first)
+            destroy(&*first);
+    }
 
-	// 如果是 __true_type，则什么也不做
-	template <class ForwardIterator>
-	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
+    // 如果是 __true_type，则什么也不做
+    template <class ForwardIterator>
+    inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
 
-	// 如果销毁的对象是指针，则什么也不做
-	inline void destroy(char*, char*) {}
-	inline void destroy(wchar_t*, wchar_t*) {}
-	inline void destroy(int*, int*) {}
-	inline void destroy(long*, long*) {}
-	inline void destroy(float*, float*) {}
-	inline void destroy(double*, double*) {}
+    // 如果销毁的对象是指针，则什么也不做
+    inline void destroy(char*, char*) {}
+    inline void destroy(wchar_t*, wchar_t*) {}
+    inline void destroy(int*, int*) {}
+    inline void destroy(long*, long*) {}
+    inline void destroy(float*, float*) {}
+    inline void destroy(double*, double*) {}
 }
 #endif // !MYTINYSTL_CONSTRUCT_H_
