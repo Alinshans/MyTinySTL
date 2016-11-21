0882846b6fc0c9a89a740af6e08a81305ad08b67
刘俊延 <13925588730@163.com>
2016-11-21 21:32:39 +0800

    clean up

--- MyTinySTL/hash_functional.h
+++ MyTinySTL/hash_functional.h
@@ -7,62 +7,67 @@
 
 namespace mystl {
 
-	// 对于大部分类型，hash function 什么都不做
-	template <class Key>
-	struct hash {};
+    // 对于大部分类型，hash function 什么都不做
+    template <class Key>
+    struct hash {};
 
-	// 针对 char* 和 const char* 设计的函数
-	inline size_t __hash_string(const char* s) {
-		unsigned long h = 0;
-		for (; *s; ++s)
-			h = 3 * h + *s;
-		return static_cast<size_t>(h);
-	}
+    // 针对 char* 和 const char* 的特化版本
+    inline size_t __hash_string(const char* s) {
+        unsigned long h = 0;
+        for (; *s; ++s)
+            h = 3 * h + *s;
+        return static_cast<size_t>(h);
+    }
 
-	template<> struct hash<char*> {
-		size_t operator()(const char* s) const { return __hash_string(s); }
-	};
+    template<> struct hash<char*> {
+        size_t operator()(const char* s) const { return __hash_string(s); }
+    };
 
-	template<> struct hash<const char*> {
-		size_t operator()(const char* s) const { return __hash_string(s); }
-	};
+    template<> struct hash<const char*> {
+        size_t operator()(const char* s) const { return __hash_string(s); }
+    };
+    
+    // 针对 mystl::string 的特化版本
+    template<> struct hash<mystl::string> {
+        size_t operator()(const mystl::string& str) const { return __hash_string(str.data()); }
+    };
 
-	// 针对 char，int，long 等整数型别，只是返回原值
+    // 针对 char，int，long 等整数型别，只是返回原值
 
-	template <> struct hash<char> {
-		size_t operator()(char x) const { return x; }
-	};
+    template <> struct hash<char> {
+        size_t operator()(char x) const { return x; }
+    };
 
-	template <> struct hash<unsigned char> {
-		size_t operator()(unsigned char x) const { return x; }
-	};
+    template <> struct hash<unsigned char> {
+        size_t operator()(unsigned char x) const { return x; }
+    };
 
-	template <> struct hash<signed char> {
-		size_t operator()(unsigned char x) const { return x; }
-	};
+    template <> struct hash<signed char> {
+        size_t operator()(unsigned char x) const { return x; }
+    };
 
-	template <> struct hash<short> {
-		size_t operator()(short x) const { return x; }
-	};
+    template <> struct hash<short> {
+        size_t operator()(short x) const { return x; }
+    };
 
-	template <> struct hash<unsigned short> {
-		size_t operator()(unsigned short x) const { return x; }
-	};
+    template <> struct hash<unsigned short> {
+        size_t operator()(unsigned short x) const { return x; }
+    };
 
-	template <> struct hash<int> {
-		size_t operator()(int x) const { return x; }
-	};
+    template <> struct hash<int> {
+        size_t operator()(int x) const { return x; }
+    };
 
-	template <> struct hash<unsigned int> {
-		size_t operator()(unsigned int x) const { return x; }
-	};
+    template <> struct hash<unsigned int> {
+        size_t operator()(unsigned int x) const { return x; }
+    };
 
-	template <> struct hash<long> {
-		size_t operator()(long x) const { return x; }
-	};
+    template <> struct hash<long> {
+        size_t operator()(long x) const { return x; }
+    };
 
-	template <> struct hash<unsigned long> {
-		size_t operator()(unsigned long x) const { return x; }
-	};
+    template <> struct hash<unsigned long> {
+        size_t operator()(unsigned long x) const { return x; }
+    };
 }
 #endif // !MYTINYSTL_HASH_FUNCTIONAL_H_
