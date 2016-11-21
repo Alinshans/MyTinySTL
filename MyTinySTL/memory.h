7be5d87021f19ef832094dcd2983885cf9dff741
刘俊延 <13925588730@163.com>
2016-11-21 21:39:05 +0800

    clean up

--- MyTinySTL/memory.h
+++ MyTinySTL/memory.h
@@ -8,62 +8,62 @@
 #include "construct.h"
 #include "uninitialized.h"
 
-namespace MyTinySTL {
-	
-	// 模板类: auto_ptr
-	// 一个小型的智能指针
-	template <class T>
-	class auto_ptr {
-	public:
-		typedef T	elem_type;
+namespace mystl {
+    
+    // 模板类: auto_ptr
+    // 一个小型的智能指针
+    template <class T>
+    class auto_ptr {
+    public:
+        typedef T    elem_type;
 
-	private:
-		T* m_ptr;	//指针
+    private:
+        T* m_ptr;    //指针
 
-	public:
-		// 构造、复制、析构函数
-		explicit auto_ptr(T* p = nullptr) :m_ptr(p) {}
-		auto_ptr(auto_ptr& x) :m_ptr(x.release()) {}
-		template <class T1>
-		auto_ptr(auto_ptr<T1>& x) : m_ptr(x.release()) {}
+    public:
+        // 构造、复制、析构函数
+        explicit auto_ptr(T* p = nullptr) :m_ptr(p) {}
+        auto_ptr(auto_ptr& x) :m_ptr(x.release()) {}
+        template <class T1>
+        auto_ptr(auto_ptr<T1>& x) : m_ptr(x.release()) {}
 
-		auto_ptr& operator=(auto_ptr& x) {
-			if (this != &x) {
-				delete m_ptr;
-				m_ptr = x.release();
-			}
-			return *this;
-		}
+        auto_ptr& operator=(auto_ptr& x) {
+            if (this != &x) {
+                delete m_ptr;
+                m_ptr = x.release();
+            }
+            return *this;
+        }
 
-		template <class T1>
-		auto_ptr& operator=(auto_ptr<T1>& x) {
-			if (this->get() != x.get()) {
-				delete m_ptr;
-				m_ptr = x.release();
-			}
-			return *this;
-		}
+        template <class T1>
+        auto_ptr& operator=(auto_ptr<T1>& x) {
+            if (this->get() != x.get()) {
+                delete m_ptr;
+                m_ptr = x.release();
+            }
+            return *this;
+        }
 
-		~auto_ptr() { delete m_ptr; }	
+        ~auto_ptr() { delete m_ptr; }    
 
-	public:
-		// 重载 operator* 和 operator->
-		T& operator*() const { return *m_ptr; }
-		T* operator->() const { return m_ptr; }
+    public:
+        // 重载 operator* 和 operator->
+        T& operator*() const { return *m_ptr; }
+        T* operator->() const { return m_ptr; }
 
-		T* get() const { return m_ptr; }
-		T* release() {
-			T* tmp = m_ptr;
-			m_ptr = nullptr;
-			return tmp;
-		}
-		void reset(T* p = nullptr) {
-			if (m_ptr != p) {
-				delete m_ptr;
-				m_ptr = p;
-			}
-		}
-	};
+        T* get() const { return m_ptr; }
+        T* release() {
+            T* tmp = m_ptr;
+            m_ptr = nullptr;
+            return tmp;
+        }
+        void reset(T* p = nullptr) {
+            if (m_ptr != p) {
+                delete m_ptr;
+                m_ptr = p;
+            }
+        }
+    };
 }
 #endif // !MYTINYSTL_MEMORY_H_
 
