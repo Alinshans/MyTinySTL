c9ea20f580cd330f0b5d76c78ec64109e1202201
刘俊延 <13925588730@163.com>
2016-11-25 23:31:55 +0800

    clean up

--- MyTinySTL/construct.h
+++ MyTinySTL/construct.h
@@ -2,7 +2,8 @@
 #define MYTINYSTL_CONSTRUCT_H_
 
 // 这个头文件包含两个全局函数 construct，destroy
-// construct 负责对象的构造，destroy 负责对象的析构
+// construct 负责对象的构造
+// destroy   负责对象的析构
 
 #include <new>
 
@@ -14,23 +15,23 @@ namespace mystl {
     // construct
     // 构造对象的内容
     // 版本1：接受一个指针
-    // 版本2：接受一个指针和初始化值
+    // 版本2：接受一个指针和初值
     template <class T1>
-    inline void construct(T1 *p) {
-        new ((void*)p) T1();
+    inline void construct(T1* p) {
+        new (static_cast<void*>(p)) T1();
     }
 
     template <class T1, class T2>
-    inline void construct(T1 *p, const T2 &value) {
-        new ((void*)p) T1(value);
+    inline void construct(T1* p, const T2& value) {
+        new (static_cast<void*>(p)) T1(value);
     }
 
     // destroy
     // 将对象析构
     // 版本1：接受一个指针
-    // 版本2：接受两个迭代器，根据元素型别，采用最适合的方法析构[first,last)区间的对象
+    // 版本2：接受两个迭代器，指向析构区间首尾
     template <class T>
-    inline void destroy(T * pointer) {
+    inline void destroy(T* pointer) {
         pointer->~T();
     }
 
@@ -51,7 +52,7 @@ namespace mystl {
             destroy(&*first);
     }
 
-    // 如果是 __true_type，则什么也不做
+    // 如果是对象有 trivial destructor，则什么也不做
     template <class ForwardIterator>
     inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
 
