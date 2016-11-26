9254f3e3dfc58a10b6173f03a1b4aea402381363
刘俊延 <13925588730@163.com>
2016-11-26 20:12:27 +0800

    Standardization

--- MyTinySTL/algorithm.h
+++ MyTinySTL/algorithm.h
@@ -1,7 +1,7 @@
 #ifndef MYTINYSTL_ALGORITHM_H_
 #define MYTINYSTL_ALGORITHM_H_
 
-// 这个头文件包含了 mystl 的所有算法，包括基础算法，数值算法，heap算法，set算法和其他算法
+// 这个头文件包含了 mystl 的所有算法，包括基本算法，数值算法，heap 算法，set 算法和其他算法
 
 #include "algo.h"
 #include "algobase.h"
@@ -11,7 +11,6 @@
 
 namespace mystl {
     
-}
-
+} // namespace mystl
 #endif // !MYTINYSTL_ALGORITHM_H_
 
