3641ea17ead8aa2e672511a000860fe1881e2ac7
刘俊延 <13925588730@163.com>
2016-11-28 22:50:42 +0800

    Standardization

--- MyTinySTL/stack.h
+++ MyTinySTL/stack.h
@@ -2,90 +2,97 @@
 #define MYTINYSTL_STACK_H_
 
 // 这个头文件包含了一个模板类 stack
-// stack 是一个后进先出的数据结构 
+// stack : 栈
 
 #include "deque.h"    
 
 namespace mystl {
 
-    // 模板类 stack
-    // 参数一代表数据类型，参数局代表容器类型，缺省使用 deque 作为底层容器
-    template <class T, class Sequence = mystl::deque<T>>
-    class stack {
-    public:
-        // 使用底层容器的型别
-        typedef typename Sequence::value_type         value_type;
-        typedef typename Sequence::size_type          size_type;
-        typedef typename Sequence::reference          reference;
-        typedef typename Sequence::const_reference    const_reference;
+// 模板类 stack
+// 参数一代表数据类型，参数二代表底层容器类型，缺省使用 deque 作为底层容器
+template <class T, class Sequence = mystl::deque<T>>
+class stack {
+  public:
+    // 使用底层容器的型别
+    typedef typename Sequence::value_type         value_type;
+    typedef typename Sequence::size_type          size_type;
+    typedef typename Sequence::reference          reference;
+    typedef typename Sequence::const_reference    const_reference;
         
-    private:
-        Sequence c_;    //底层容器
+  private:
+    Sequence s_;  // 用底层容器表现 stack
 
-    public:
-        // 构造、复制、移动函数
-        stack() :c_() {}
-        stack(size_type n, const T& value) :c_(n, value) {}
-        explicit stack(size_type n) :c_(n) {}
-        template <class InputIterator>
-        stack(InputIterator first, InputIterator last) : c_(first, last) {}
+  public:
+    // 构造、复制、移动函数
+    stack() :s_() {}
+    explicit stack(size_type n) :s_(n) {}
+    stack(size_type n, const value_type& value) :s_(n, value) {}
+    template <class InputIterator>
+    stack(InputIterator first, InputIterator last) : s_(first, last) {}
 
-        explicit stack(const Sequence& s) :c_(s) {}
-        explicit stack(Sequence&& s) :c_(std::move(s)) {}
+    explicit stack(const Sequence& s) :s_(s) {}
+    explicit stack(Sequence&& s) :s_(std::move(s)) {}
 
-        stack(const stack& rhs) :c_(rhs.c_) {}
-        stack(stack&& rhs) :c_(std::move(rhs.c_)) {}
+    stack(const stack& rhs) :s_(rhs.s_) {}
+    stack(stack&& rhs) :s_(std::move(rhs.s_)) {}
 
-        stack& operator=(const stack& rhs) { c_ = rhs.c_; return *this; }
-        stack& operator=(stack&& rhs) { c = std::move(rhs.c_); return *this; }
+    stack& operator=(const stack& rhs) { s_ = rhs.s_; return *this; }
+    stack& operator=(stack&& rhs) { c = std::move(rhs.s_); return *this; }
 
-        // 以下操作使用底层容器的操作
-        bool empty() const { return c_.empty(); }
-        size_type size() const { return c_.size(); }
+    // 以下操作使用底层容器的操作
+    bool            empty()                        const { return s_.empty(); }
+    size_type       size()                         const { return s_.size(); }
 
-        reference top() { return c_.back(); }
-        const_reference top() const { return c_.back(); }
-        void push(const T& x) { c_.push_back(x); }
-        void pop() { c_.pop_back(); } 
+    reference       top()                                { return s_.back(); }
+    const_reference top()                          const { return s_.back(); }
+    void            push(const value_type& value)        { s_.push_back(value); }
+    void            pop()                                { s_.pop_back(); } 
 
-        void clear() { while (!empty())    pop(); }
-        void swap(stack& rhs) { mystl::swap(c_, rhs.c_); }
+    void clear()                                         { while (!empty())  pop(); }
+    void swap(stack& rhs)                                { mystl::swap(s_, rhs.s_); }
 
-    public:
-        friend bool operator==(const stack& lhs, const stack& rhs) {
-            return lhs.c_ == rhs.c_;
-        }
-        friend bool operator<(const stack& lhs, const stack& rhs) {
-            return lhs.c_ < rhs.c_;
-        }
-    };
+  public:
+    friend bool operator==(const stack& lhs, const stack& rhs) { return lhs.s_ == rhs.s_; }
+    friend bool operator< (const stack& lhs, const stack& rhs) { return lhs.s_ <  rhs.s_; }
+};
 
-    // 重载比较操作符
-    template <class T, class Sequence>
-    bool operator!=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
-        return !(lhs == rhs);
-    }
-
-    template <class T, class Sequence>
-    bool operator>(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
-        return rhs < lhs;
-    }
-
-    template <class T, class Sequence>
-    bool operator<=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
-        return !(rhs < lhs);
-    }
-
-    template <class T, class Sequence>
-    bool operator>=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
-        return !(lhs < rhs);
-    }
-    
-    // 重载 mystl 的 swap
-    template <class T, class Sequence>
-    void swap(stack<T, Sequence>& lhs, stack<T, Sequence>& rhs) {
-        lhs.swap(rhs);
-    }
+// 重载比较操作符
+template <class T, class Sequence>
+bool operator==(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return lhs == rhs;
 }
+
+template <class T, class Sequence>
+bool operator<(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return lhs < rhs;
+}
+
+template <class T, class Sequence>
+bool operator!=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <class T, class Sequence>
+bool operator>(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return rhs < lhs;
+}
+
+template <class T, class Sequence>
+bool operator<=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return !(rhs < lhs);
+}
+
+template <class T, class Sequence>
+bool operator>=(const stack<T, Sequence>& lhs, const stack<T, Sequence>& rhs) {
+    return !(lhs < rhs);
+}
+    
+// 重载 mystl 的 swap
+template <class T, class Sequence>
+void swap(stack<T, Sequence>& lhs, stack<T, Sequence>& rhs) {
+    lhs.swap(rhs);
+}
+
+} // namespace mystl
 #endif // !MYTINYSTL_STACK_H_
 
