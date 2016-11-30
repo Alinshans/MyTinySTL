9c13167cd4d5f2cc07487d58cc81c9d3e3ee350d
刘俊延 <13925588730@163.com>
2016-11-30 23:48:51 +0800

    Refactoring

--- MyTinySTL/Test/algorithm_performance_test.h
+++ MyTinySTL/Test/algorithm_performance_test.h
@@ -6,90 +6,94 @@
 #include <algorithm>
 
 #include "../algorithm.h"
+#include "test.h"
 
 namespace mystl {
-    namespace test {
-        namespace algorithm_performance_test {
+namespace test {
+namespace algorithm_performance_test
+{
 
-            // 第一种函数类型测试
-            #define FUN_TEST1(mode, fun, count) do { \
-                std::string fun_name = #fun; \
-                srand((int)time(0)); \
-                char buf[10]; \
-                clock_t start, end; \
-                int *arr = new int[count]; \
-                for(size_t i = 0; i < count; ++i)    *(arr + i) = rand(); \
-                start = clock(); \
-                mode::fun(arr, arr + count); \
-                end = clock(); \
-                int n = end - start; \
-                sprintf_s(buf, "%d", n); \
-                std::string t = buf; \
-                t += "ms   |"; \
-                std::cout << std::setw(WIDE) << t; \
-                delete []arr; \
-            } while(0)
+// 函数性能测试宏定义
+#define FUN_TEST1(mode, fun, count) do {                      \
+    std::string fun_name = #fun;                               \
+    srand((int)time(0));                                       \
+    char buf[10];                                              \
+    clock_t start, end;                                        \
+    int *arr = new int[count];                                 \
+    for(size_t i = 0; i < count; ++i)  *(arr + i) = rand();    \
+    start = clock();                                           \
+    mode::fun(arr, arr + count);                               \
+    end = clock();                                             \
+    int n = static_cast<int>(static_cast<double>(end - start)  \
+        / CLOCKS_PER_SEC * 1000);                              \
+    sprintf_s(buf, "%d", n);                                   \
+    std::string t = buf;                                       \
+    t += "ms   |";                                             \
+    std::cout << std::setw(WIDE) << t;                         \
+    delete []arr;                                              \
+} while(0)
 
-            // 第二种函数类型测试
-            #define FUN_TEST2(mode, fun, count) do { \
-                std::string fun_name = #fun; \
-                srand((int)time(0)); \
-                char buf[10]; \
-                clock_t start, end; \
-                int *arr = new int[count]; \
-                for(size_t i = 0; i < count; ++i)    *(arr + i) = rand(); \
-                start = clock(); \
-                for(size_t i = 0; i < count; ++i) \
-                    mode::fun(arr, arr + count, rand()); \
-                end = clock(); \
-                int n = end - start; \
-                sprintf_s(buf, "%d", n); \
-                std::string t = buf; \
-                t += "ms   |"; \
-                std::cout << std::setw(WIDE) << t; \
-                delete []arr; \
-            } while(0)
+#define FUN_TEST2(mode, fun, count) do {                      \
+    std::string fun_name = #fun;                               \
+    srand((int)time(0));                                       \
+    char buf[10];                                              \
+    clock_t start, end;                                        \
+    int *arr = new int[count];                                 \
+    for(size_t i = 0; i < count; ++i)  *(arr + i) = rand();    \
+    start = clock();                                           \
+    for(size_t i = 0; i < count; ++i)                          \
+        mode::fun(arr, arr + count, rand());                   \
+    end = clock();                                             \
+    int n = static_cast<int>(static_cast<double>(end - start)  \
+        / CLOCKS_PER_SEC * 1000);                              \
+    sprintf_s(buf, "%d", n);                                   \
+    std::string t = buf;                                       \
+    t += "ms   |";                                             \
+    std::cout << std::setw(WIDE) << t;                         \
+    delete []arr;                                              \
+} while(0)
 
-            void binary_search_test() {
-                std::cout << "[------------------- function : binary_search ------------------]" << std::endl;
-                std::cout << "| orders of magnitude |";
-                TEST_LEN(LEN2, LEN3, LEN4, WIDE);
-                std::cout << "|         std         |";
-                FUN_TEST2(std, binary_search, LEN2);
-                FUN_TEST2(std, binary_search, LEN3);
-                FUN_TEST2(std, binary_search, LEN4);
-                std::cout << std::endl << "|        mystl        |";
-                FUN_TEST2(mystl, binary_search, LEN2);
-                FUN_TEST2(mystl, binary_search, LEN3);
-                FUN_TEST2(mystl, binary_search, LEN4);
-                std::cout << std::endl;
-            }
-    
-            void sort_test() {
-                std::cout << "[----------------------- function : sort -----------------------]" << std::endl;
-                std::cout << "| orders of magnitude |";
-                TEST_LEN(LEN2, LEN3, LEN4, WIDE);
-                std::cout << "|         std         |";
-                FUN_TEST1(std, sort, LEN2);
-                FUN_TEST1(std, sort, LEN3);
-                FUN_TEST1(std, sort, LEN4);
-                std::cout << std::endl << "|        mystl        |";
-                FUN_TEST1(mystl, sort, LEN2);
-                FUN_TEST1(mystl, sort, LEN3);
-                FUN_TEST1(mystl, sort, LEN4);
-                std::cout << std::endl;
-            }
-
-            void algorithm_performance_test() {
-                std::cout << "[===============================================================]" << std::endl;
-                std::cout << "[--------------- Run algorithm performance test ----------------]" << std::endl;
-                sort_test();
-                binary_search_test();
-                std::cout << "[--------------- End algorithm performance test ----------------]" << std::endl;
-                std::cout << "[===============================================================]" << std::endl;
-            }
-        }
-    }
+void binary_search_test() {
+    std::cout << "[------------------- function : binary_search ------------------]" << std::endl;
+    std::cout << "| orders of magnitude |";
+    TEST_LEN(LEN2, LEN3, LEN4, WIDE);
+    std::cout << "|         std         |";
+    FUN_TEST2(std, binary_search, LEN2);
+    FUN_TEST2(std, binary_search, LEN3);
+    FUN_TEST2(std, binary_search, LEN4);
+    std::cout << std::endl << "|        mystl        |";
+    FUN_TEST2(mystl, binary_search, LEN2);
+    FUN_TEST2(mystl, binary_search, LEN3);
+    FUN_TEST2(mystl, binary_search, LEN4);
+    std::cout << std::endl;
 }
+    
+void sort_test() {
+    std::cout << "[----------------------- function : sort -----------------------]" << std::endl;
+    std::cout << "| orders of magnitude |";
+    TEST_LEN(LEN2, LEN3, LEN4, WIDE);
+    std::cout << "|         std         |";
+    FUN_TEST1(std, sort, LEN2);
+    FUN_TEST1(std, sort, LEN3);
+    FUN_TEST1(std, sort, LEN4);
+    std::cout << std::endl << "|        mystl        |";
+    FUN_TEST1(mystl, sort, LEN2);
+    FUN_TEST1(mystl, sort, LEN3);
+    FUN_TEST1(mystl, sort, LEN4);
+    std::cout << std::endl;
+}
+
+void algorithm_performance_test() {
+    std::cout << "[===============================================================]" << std::endl;
+    std::cout << "[--------------- Run algorithm performance test ----------------]" << std::endl;
+    sort_test();
+    binary_search_test();
+    std::cout << "[--------------- End algorithm performance test ----------------]" << std::endl;
+    std::cout << "[===============================================================]" << std::endl;
+}
+
+} // namespace algorithm_performance_test
+} // namespace test
+} // namespace mystl
 #endif // !MYTINYSTL_ALGORITHM_PERFORMANCE_TEST_H_
 
