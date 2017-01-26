c6e8d74475eff5ba476c1176094e50c42af59da2
刘俊延 <13925588730@163.com>
2017-01-26 15:46:06 +0800

    Add the control of the test

--- MyTinySTL/Test/algorithm_performance_test.h
+++ MyTinySTL/Test/algorithm_performance_test.h
@@ -56,40 +56,44 @@ namespace algorithm_performance_test
 void binary_search_test() {
     std::cout << "[------------------- function : binary_search ------------------]" << std::endl;
     std::cout << "| orders of magnitude |";
-    TEST_LEN(LEN2, LEN3, LEN4, WIDE);
+    TEST_LEN(LEN1, LEN2, LEN3, WIDE);
     std::cout << "|         std         |";
+    FUN_TEST2(std, binary_search, LEN1);
     FUN_TEST2(std, binary_search, LEN2);
     FUN_TEST2(std, binary_search, LEN3);
-    FUN_TEST2(std, binary_search, LEN4);
     std::cout << std::endl << "|        mystl        |";
+    FUN_TEST2(mystl, binary_search, LEN1);
     FUN_TEST2(mystl, binary_search, LEN2);
     FUN_TEST2(mystl, binary_search, LEN3);
-    FUN_TEST2(mystl, binary_search, LEN4);
     std::cout << std::endl;
 }
     
 void sort_test() {
     std::cout << "[----------------------- function : sort -----------------------]" << std::endl;
     std::cout << "| orders of magnitude |";
-    TEST_LEN(LEN2, LEN3, LEN4, WIDE);
+    TEST_LEN(LEN1, LEN2, LEN3, WIDE);
     std::cout << "|         std         |";
+    FUN_TEST1(std, sort, LEN1);
     FUN_TEST1(std, sort, LEN2);
     FUN_TEST1(std, sort, LEN3);
-    FUN_TEST1(std, sort, LEN4);
     std::cout << std::endl << "|        mystl        |";
+    FUN_TEST1(mystl, sort, LEN1);
     FUN_TEST1(mystl, sort, LEN2);
     FUN_TEST1(mystl, sort, LEN3);
-    FUN_TEST1(mystl, sort, LEN4);
     std::cout << std::endl;
 }
 
 void algorithm_performance_test() {
+
+#if PERFORMANCE_TEST_ON
     std::cout << "[===============================================================]" << std::endl;
     std::cout << "[--------------- Run algorithm performance test ----------------]" << std::endl;
     sort_test();
     binary_search_test();
     std::cout << "[--------------- End algorithm performance test ----------------]" << std::endl;
     std::cout << "[===============================================================]" << std::endl;
+#endif // PERFORMANCE_TEST_ON
+
 }
 
 } // namespace algorithm_performance_test
