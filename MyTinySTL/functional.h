6253d864fe72eadbf1d50b64b4c320034893d3ac
刘俊延 <13925588730@163.com>
2017-01-30 15:11:32 +0800

    v1.1.0

--- MyTinySTL/functional.h
+++ MyTinySTL/functional.h
@@ -3,6 +3,10 @@
 
 // 这个头文件包含了 mystl 的函数对象
 
+#include <cstddef>
+
+#include "string.h"
+
 namespace mystl {
 
 // 定义一元函数的参数型别和返回值型别
@@ -150,6 +154,71 @@ struct projectfirst :public binary_function<Arg1, Arg2, Arg1> {
 template <class Arg1, class Arg2>
 struct projectsecond :public binary_function<Arg1, Arg2, Arg1> {
     Arg2 operator()(const Arg1&, const Arg2& y) const { return y; }
+};
+
+// 哈希函数对象
+
+// 对于大部分类型，hash function 什么都不做
+template <class Key>
+struct hash {};
+
+// 针对 char* 和 const char* 的特化版本
+inline size_t __hash_string(const char* s) {
+    size_t h = 0;
+    for (; *s; ++s)
+        h = 3 * h + static_cast<size_t>(*s);
+    return h;
+}
+
+template <> struct hash<char*> {
+    size_t operator()(const char* s) const { return __hash_string(s); }
+};
+
+template <> struct hash<const char*> {
+    size_t operator()(const char* s) const { return __hash_string(s); }
+};
+    
+// 针对 mystl::string 的特化版本
+template <> struct hash<mystl::string> {
+    size_t operator()(const mystl::string& str) const { return __hash_string(str.data()); }
+};
+
+// 针对 char，int，long 等整数型别，只是返回原值
+
+template <> struct hash<char> {
+    size_t operator()(char x) const { return x; }
+};
+
+template <> struct hash<unsigned char> {
+    size_t operator()(unsigned char x) const { return x; }
+};
+
+template <> struct hash<signed char> {
+    size_t operator()(unsigned char x) const { return x; }
+};
+
+template <> struct hash<short> {
+    size_t operator()(short x) const { return x; }
+};
+
+template <> struct hash<unsigned short> {
+    size_t operator()(unsigned short x) const { return x; }
+};
+
+template <> struct hash<int> {
+    size_t operator()(int x) const { return x; }
+};
+
+template <> struct hash<unsigned int> {
+    size_t operator()(unsigned int x) const { return x; }
+};
+
+template <> struct hash<long> {
+    size_t operator()(long x) const { return x; }
+};
+
+template <> struct hash<unsigned long> {
+    size_t operator()(unsigned long x) const { return x; }
 };
 
 } // namespace mystl
