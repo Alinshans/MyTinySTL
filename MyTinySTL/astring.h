df31819a375c88d3b4e3704b8b4284a5cbbec6e1
Alinshans <13925588730@163.com>
2017-08-16 18:03:19 +0800

    :star: Fix typo.

--- MyTinySTL/astring.h
+++ MyTinySTL/astring.h
@@ -1,17 +1,17 @@
 ﻿#ifndef MYTINYSTL_ASTRING_H_
 #define MYTINYSTL_ASTRING_H_
 
-// 定义了 string, wstring 类型
+// 定义了 string, wstring, u16string, u32string 类型
 
 #include "basic_string.h"
 
 namespace mystl
 {
 
-using string     = mystl::basic_string<char>;
-using wstring    = mystl::basic_string<wchar_t>;
-using u16string  = mystl::basic_string<char16_t>;
-using u32wstring = mystl::basic_string<char32_t>;
+using string    = mystl::basic_string<char>;
+using wstring   = mystl::basic_string<wchar_t>;
+using u16string = mystl::basic_string<char16_t>;
+using u32string = mystl::basic_string<char32_t>;
 
 }
 #endif // !MYTINYSTL_ASTRING_H_
